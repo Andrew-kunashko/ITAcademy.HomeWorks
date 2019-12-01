e8c4d09c44ffbe59b93a5e6832845c5a4066bb3b
Andrew Kunashko <dadaya10@mail.ru>
2019-12-02 00:22:57 +0300

    added code to method main

--- blank/HW.02/Program.cs
+++ blank/HW.02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace HW._02
 {
@@ -6,14 +7,18 @@ namespace HW._02
     {
         static void Main(string[] args)
         {
-            //1.Указываем путь к файлу из которого будет считываться текст
-            //2.Считываем содержимое файла в строку 
-            //3.разбиваем полученную строку на массив символов
-            //4.создаем массив байтов и задаем его длину
-            //5.обрабатываем в цикле каждый символ файла
-            //6.преобразуем текущий символ файла к типу byte
-            //7.присваиваем полученное значение текущему байту изображения
-            //8.записываем полученные байты в наш конечный результат - картинку
+            StreamReader textReader = new StreamReader(@"E:/TASK/image.txt", true); 
+            string textReaderResult = textReader.ReadToEnd(); 
+            string[] arrayOfTextResult = textReaderResult.Split(' '); 
+            byte[] imageBytes = new byte[arrayOfTextResult.Length - 1];
+            textReader.Dispose();
+
+            for (int i = 0; i < arrayOfTextResult.Length - 1; i++) 
+            {
+                byte binary = Convert.ToByte(arrayOfTextResult[i], 2); 
+                imageBytes[i] = binary; 
+            }
+            File.WriteAllBytes(@"E:/TASK/image.png", imageBytes); 
         }
     }
 }
