c7fba4ddc83d58e064a580b02e728a59f78cda83
Andrew Kunashko <dadaya10@mail.ru>
2019-12-15 20:06:29 +0300

    added homework 4

---
+++ task1/hm_04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.IO;
+namespace hm_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            for(    char i = 'z'; i >='a'; i--)
+
+            {
+                
+                Console.WriteLine(i);
+            }
+            
+        }
+    }
+}
---
+++ task2/hm_04/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace hm_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите символ");
+            char simvol = Convert.ToChar(Console.ReadLine());
+            if(simvol == 'W')
+            {
+                Console.WriteLine("Фигура переместилась вверх");
+            }
+            if (simvol == 'S')
+            {
+                Console.WriteLine("Фигура переместилась вниз");
+            }
+            if (simvol == 'A')
+            {
+                Console.WriteLine("Фигура переместилась влево");
+            }
+            if (simvol == 'D')
+            {
+                Console.WriteLine("Фигура переместилась вправо");
+            }
+            if(simvol != 'W' && simvol!='A' && simvol!='S' && simvol!='D' )
+            {
+                Console.WriteLine("Нет необходимости перемещения");
+            }
+
+
+                
+        }
+    }
+}
